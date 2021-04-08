fc179b221f999c984e9790f7c5379fea9bf25dfc
zachary yu <zachary.yu@kneron.com>
2021-04-08 22:49:43 +0800

    revise tool seqsplit.py

--- seqsplit.py
+++ seqsplit.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
 
 import sys
+import re
 
-print('Number of arguments:' + str(len(sys.argv)) +  ' arguments.')
-print('Argument List:' + str(sys.argv))
+#print('Number of arguments:' + str(len(sys.argv)) +  ' arguments.')
+#print('Argument List:' + str(sys.argv))
 
-iFile = sys.argv[1]
-(rows, cols, chs) = sys.argv[2].split('x')
-(r_st, r_end, c_st, c_end, ch_st, ch_end) = sys.argv[3].split(',')
-oFile = sys.argv[1] + "_" + rows + "x" + cols + "x" + chs + "_" + r_st + "." + c_st + "." + ch_st
-rows = int(rows)
-cols = int(cols)
-chs = int(chs)
-r_st = int(r_st)
-c_st = int(c_st)
-ch_st = int(ch_st)
-r_end = int(r_end)
-c_end = int(c_end)
-ch_end = int(ch_end)
-print("input: " + iFile)
-print("output " + oFile)
-print(str(rows) + " " + str(cols) + " " + str(chs))
-"""
 try:
+    iFile = sys.argv[1]
+    (rows, cols, chs) = sys.argv[2].split('x')
+    (r_st, r_end, c_st, c_end, ch_st, ch_end) = sys.argv[3].split(',')
+    oFile = sys.argv[1] + "_" + rows + "x" + cols + "x" + chs + "_" + r_st + "." + r_end + "." + c_st + "." + c_end + "." + ch_st + "." + ch_end
+    rows = int(rows)
+    cols = int(cols)
+    chs = int(chs)
+    r_st = int(r_st)
+    c_st = int(c_st)
+    ch_st = int(ch_st)
+    r_end = int(r_end)
+    c_end = int(c_end)
+    ch_end = int(ch_end)
+    print("input: " + iFile)
+    print("output: " + oFile)
+
+    count = 0
     with open(iFile, 'r') as ifd:
-        with open(oFile, 'wb') as ofd: 
-            for x in ifd:
-                b = [int('0x' + str(x[i:i+2]), 16) for i in range(lineLen) if i % 2 == 0]
-                b.reverse()
-                
-                ofd.write(bytes(b))
+        fd = ifd.read()
+        fd = re.split('\s+', fd)
+        with open(oFile, 'w') as ofd: 
+            for ch in range (ch_st, ch_end):
+                for r in range (r_st, r_end):
+                    offset = ch*rows*cols + r*cols
+                    flag = 0
+                    for c in range (c_st, c_end):
+                        ofd.write(str(fd[offset + c] + "\n"));
+                        #if flag == 0:
+                        #    print(str(count) + " " + str(offset + c) + " " + fd[offset + c])
+                        #    print(str(r) + " " + str(c) + " " + str(ch));
+                        flag = flag + 1
+                        count = count + 1
+
+    print("total count = ", count)
+    print("............. [done]")
 except:
-    print("usage: weightHex2Bin.py input.hex output.bin")
-"""  
+    print("""usage: seqsplit ROWSxCOLSxCHS r_st,r_end,c_st,c_end,ch_st,ch_end
+(x_st is inclsive, x_end is exclusive)
+    """)
