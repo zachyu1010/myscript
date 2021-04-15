2257ed5efa5cc5894f796583d608ece2cf183f81
zachary yu <zachary.yu@kneron.com>
2021-04-15 16:41:53 +0800

    [weightHex2Bin] replace 'X' to '0'

--- weightHex2Bin.py
+++ weightHex2Bin.py
@@ -11,18 +11,19 @@ try:
 
     lineLen = 32
     with open(iFile, 'r') as ifd:
-        with open(oFile, 'wb') as ofd: 
+        with open(oFile, 'wb') as ofd:
             for x in ifd:
+                x = x.replace('X', '0')
                 b = [int('0x' + str(x[i:i+2]), 16) for i in range(lineLen) if i % 2 == 0]
                 b.reverse()
-                
+
                 ofd.write(bytes(b))
-    
+
 except:
     print("usage: weightHex2Bin.py input.hex output.bin")
-            
-        
-        
-            
+
+
+
+
 
 
